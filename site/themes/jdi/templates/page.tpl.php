5d21fedf1bd1f0a53ad4473fcdb087437674ece4
Danillo Nunes <mdanillo@gmail.com>
2011-05-23 00:04:09 -0300

    Remove $feed_icons from page.tpl.php

--- site/themes/jdi/templates/page.tpl.php
+++ site/themes/jdi/templates/page.tpl.php
@@ -141,7 +141,6 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
     </div></div><!-- /.section, /#content -->
 
     <?php if ($page['navigation'] || $main_menu): ?>
