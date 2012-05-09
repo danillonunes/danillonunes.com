fc7ef99877025387f229c188c676c41122ce3e68
Danillo Nunes <git@danillonunes.net>
2012-05-09 01:35:54 -0300

    Split settings between various files

---
+++ site/settings/development.settings.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Environment Indicator variables.
+ */
+$conf['environment_indicator_enabled']  = TRUE;
+$conf['environment_indicator_position'] = 'right';
+$conf['environment_indicator_margin']   = FALSE;
+$conf['environment_indicator_text']     = 'DEVELOPMENT';
+$conf['environment_indicator_color']    = 'red';
+
+/**
+ * Disable cache settings.
+ */
+$conf['cache']            = FALSE;
+$conf['block_cache']      = FALSE;
+$conf['page_compression'] = FALSE;
+$conf['preprocess_css']   = FALSE;
+$conf['preprocess_js']    = FALSE;
+
+/**
+ * Enable LESS developer mode.
+ */
+$conf['less_devel']       = TRUE;
---
+++ site/settings/production.settings.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Set base url for production site.
+ */
+switch ($_SERVER['HTTP_HOST']) {
+  case 'danillonunes.net':
+    $base_url = 'http://danillonunes.net';
+    $cookie_domain = 'danillonunes.net';
+    break;
+  case 'dh.danillonunes.net':
+    $base_url = 'http://dh.danillonunes.net';
+    $cookie_domain = 'dh.danillonunes.net';
+    break;
+}
+
+/**
+ * Reinvigorate variable.
+ */
+$conf['reinvigorate_account'] = 'b7dpl-4y3vm2858r';
