85d042fada10797759f396d1ba142622d16898c1
Danillo Nunes <git@danillonunes.net>
2012-05-16 17:55:24 -0300

    Fix error when try to add a setting file from a inexistent environment

--- site/settings.php
+++ site/settings.php
@@ -15,18 +15,19 @@ $environment = get_environment();
 /**
  * List of additional settings files to be included.
  */
-$settings_files = array(
-  'common',
-  $environment,
-  'secret'
-);
+$settings_files = array();
+$settings_files['common'] = 'common';
+if ($environment) {
+  $settings_files[$environment] = $environment;
+}
+$settings_files['secret'] = 'secret';
 
 /**
  * Include additional settings files.
  */
 foreach ($settings_files as $settings_file) {
   $settings_file_path = $settings_dir . '/' . $settings_file . '.settings.php';
-  require_once $settings_file_path;
+  include_once $settings_file_path;
 }
 
 /**
