cf5d8b985bb10e4d42e6cbb14302cd4a3b8ec762
Danillo Nunes <git@danillonunes.net>
2012-05-20 00:20:47 -0300

    Add assets autoload code

--- site/themes/jdi5/template.php
+++ site/themes/jdi5/template.php
@@ -2,11 +2,14 @@
 
 /**
  * @file
- * This file is empty by default because the base theme chain (Alpha & Omega) provides
- * all the basic functionality. However, in case you wish to customize the output that Drupal
- * generates through Alpha & Omega this file is a good place to do so.
- * 
- * Alpha comes with a neat solution for keeping this file as clean as possible while the code
- * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
- * for more information on this topic.
+ * Contains (pre)process and another theme related functions.
  */
+
+include_once 'includes/assets-autoload.inc';
+
+/**
+ * Implements hook_preprocess().
+ */
+function jdi5_preprocess($variables, $hook) {
+  assets_autoload($variables, $hook);
+}
