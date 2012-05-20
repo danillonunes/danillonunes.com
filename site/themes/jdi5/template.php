099a71be6a0b69a20dfa9ea1f03f0b4c40f35b9b
Danillo Nunes <git@danillonunes.net>
2012-05-20 17:18:30 -0300

    Add fix to Omega works with Respond.js

--- site/themes/jdi5/template.php
+++ site/themes/jdi5/template.php
@@ -13,3 +13,22 @@ include_once 'includes/assets-autoload.inc';
 function jdi5_preprocess($variables, $hook) {
   assets_autoload($variables, $hook);
 }
+
+/**
+ * Implements hook_css_alter().
+ */
+function jdi5_css_alter(&$css) {
+  /**
+   * Remove conditional tags for IE.
+   *
+   * This code is necessary to make Omega and Respond.js works together.
+   * Source: http://drupal.org/node/1272812#comment-5946156
+   */
+  foreach ($css as $key => $value) {
+    if (preg_match('/^ie::(\S*)/', $key)) {
+      unset($css[$key]);
+    } else {
+      $css[$key]['browsers']['IE'] = TRUE;
+    }
+  }
+}
