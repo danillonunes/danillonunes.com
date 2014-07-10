eed0e27f60a5c130beb10087798b80ed43224e3b
Danillo Nunes <git@danillonunes.net>
2014-07-10 01:21:12 -0300

    Add IP sent by CloudFlare

--- http/sites/default/settings/common.settings.php
+++ http/sites/default/settings/common.settings.php
@@ -1,3 +1,8 @@
 <?php
 
 $conf['theme_default'] = 'jdi5';
+
+// Add IP sent by CloudFlare
+if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+}
