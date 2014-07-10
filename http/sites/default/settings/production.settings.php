d62aecd9014d6d8c72c6c83eb6a89b4c7bec8b45
Danillo Nunes <git@danillonunes.net>
2014-07-10 00:39:04 -0300

    Add Memcache settings on production.settings.php

--- http/sites/default/settings/production.settings.php
+++ http/sites/default/settings/production.settings.php
@@ -25,3 +25,18 @@ $conf['reinvigorate_account'] = 'b7dpl-4y3vm2858r';
 if (defined('CDN_ENABLED')) {
   $conf['cdn_status'] = CDN_ENABLED;
 }
+
+/**
+ * Add Memcache settings.
+ */
+$conf['cache_backends'][] = 'sites/all/modules/memcache/memcache.inc';
+$conf['lock_inc'] = 'sites/all/modules/memcache/memcache-lock.inc';
+$conf['memcache_stampede_protection'] = TRUE;
+$conf['cache_default_class'] = 'MemCacheDrupal';
+
+// The 'cache_form' bin must be assigned to non-volatile storage.
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+
+// Don't bootstrap the database when serving pages from the cache.
+$conf['page_cache_without_database'] = TRUE;
+$conf['page_cache_invoke_hooks'] = FALSE;
